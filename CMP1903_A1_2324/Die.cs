7ab759823ab79eedefb40d7d17f19e5e51574d80
Svens <svens871@gmail.com>
2024-03-07 06:27:55 +0000

    New C

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -9,18 +9,18 @@ namespace CMP1903_A1_2324
     internal class Die
     {
         private Random random; // Private variable used to generate random numbers
-        public int DieRolled {get; private set;} // This is where the result of a die roll will get placed into
+        public int dieRolled {get; private set;} // This is where the result of a die roll will get placed into
 
         public Die()
         {
             random = new Random(); // Starts randomising when a Die object is created
-            DieRolled = 0; // Initial value of DieRolled is 0
+            dieRolled = 0; // Initial value of DieRolled is 0
         }
 
         public int Roll()
         {
             DieRolled = random.Next(1, 7); // Generates random number between 1 and 6, and then the DieRolled will be stored in the property waiting to be used.
-            return DieRolled;
+            return dieRolled;
         }
 
     }
